673a9529ffc8856c4ead6dcf16b530f9e8c31d5b
Olof Björn <olof.v.bjorn@gmail.com>
2026-09-08 14:47:28 +0200

    Implemented changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+//import React from 'react'
 import { Canvas } from '@react-three/fiber'
 
 export function ConfiguratorCanvas() {
   return (
     <Canvas>
+      <ambientLight intensity={0.6} />
+      <directionalLight position={[5, 5, 5]} intensity={1} />
       <mesh>
         <boxGeometry />
         <meshStandardMaterial />
