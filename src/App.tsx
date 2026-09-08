56d40b6977582ffc706c4d36eefc73619cf1725f
Olof Björn <olof.v.bjorn@gmail.com>
2026-09-08 14:54:20 +0200

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ export function ConfiguratorCanvas() {
   return (
     <Canvas>
       <ambientLight intensity={0.6} />
-      <directionalLight position={[5, 5, 5]} intensity={1} />
+      <directionalLight position={[5, 5, 5]} intensity={1}  />
       <mesh>
         <boxGeometry />
         <meshStandardMaterial />
