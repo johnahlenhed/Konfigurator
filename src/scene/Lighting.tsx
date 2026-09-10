fd0424a875bcb85b66cab3f80a15876053886ef1
mariatedeman <m.tedeman@hotmail.com>
2026-09-10 11:43:36 +0200

    Rename directionsIntensity to directionalIntensity

--- src/scene/Lighting.tsx
+++ src/scene/Lighting.tsx
@@ -1,18 +1,18 @@
 type LightingProps = {
   ambientIntensity?: number;
   directionalPosition?: [number, number, number];
-  directionsIntensity?: number;
+  directionalIntensity?: number;
 };
 
 export function Lighting({
   ambientIntensity = 0.6,
   directionalPosition = [5, 5, 5],
-  directionsIntensity = 1,
+  directionalIntensity = 1,
 }: LightingProps) {
   return (
     <>
       <ambientLight intensity={ambientIntensity} />
-      <directionalLight position={directionalPosition} intensity={directionsIntensity} />
+      <directionalLight position={directionalPosition} intensity={directionalIntensity} />
     </>
   );
 }
