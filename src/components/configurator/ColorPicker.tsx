ffac60a4ad023e05fd1efe26a1928c12f1327844
Olof Björn <olof.v.bjorn@gmail.com>
2026-09-11 17:49:05 +0200

    Minor changes before rework

--- src/components/configurator/ColorPicker.tsx
+++ src/components/configurator/ColorPicker.tsx
@@ -1,26 +1,23 @@
 import { useConfiguratorStore } from '../../store/configuratorStore';
 import { baseColors } from '../../store/configOption';
+import styles from './ColorPicker.module.css';
 
 export function ColorPicker() {
-  const baseColor = useConfiguratorStore(
-    (state) => state.selection.baseColor
-  );
-
-  const setBaseColor = useConfiguratorStore(
-    (state) => state.setBaseColor
-  );
+  const baseColor = useConfiguratorStore((state) => state.selection.baseColor);
+  const setBaseColor = useConfiguratorStore((state) => state.setBaseColor);
 
   return (
-    <div>
+    <div className={styles.row}>
       {baseColors.map((color) => (
         <button
           key={color.id}
           type="button"
+          className={styles.swatch}
+          style={{ backgroundColor: color.hex }}
           onClick={() => setBaseColor(color.id)}
           aria-pressed={baseColor === color.id}
-        >
-          {color.label}
-        </button>
+          aria-label={color.label}
+        />
       ))}
     </div>
   );
