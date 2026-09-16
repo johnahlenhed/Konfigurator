39a50dc6dbdcfff4cf7acaf49f0892e7384ce701
mariatedeman <m.tedeman@hotmail.com>
2026-09-16 18:38:04 +0200

    Updated camera setup to present front of product

--- src/scene/Scene.tsx
+++ src/scene/Scene.tsx
@@ -38,7 +38,7 @@ export function Scene() {
   return (
     <Canvas>
       <Lighting ambientIntensity={1.5} directionalIntensity={3} directionalPosition={[7, 7, 9]} />
-      <CameraSetup position={[0, 6, 10]} />
+      <CameraSetup position={[0, 0, 10]} />
       <Suspense fallback={null}>
         <SceneContent />
       </Suspense>
