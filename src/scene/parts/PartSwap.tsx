398ba0a588f1291b5244497ec1c3a4026b55ef5d
John Ahlenhed <johnahlenhed@gmail.com>
2026-09-17 17:10:54 +0200

    Update PartSwap.tsx

--- src/scene/parts/PartSwap.tsx
+++ src/scene/parts/PartSwap.tsx
@@ -9,7 +9,6 @@ import { detachPart } from "./detachPart";
 import { attachSocket } from "./attachSocket";
 import { cloneWithMaterials, getAnchorOffset } from "./getAnchorOffset";
 import { useFrame } from "@react-three/fiber";
-import { traverseMeshes } from "../../utils/modelHelpers";
 import { speakerColorSchemes } from "../materials/speakerColorSchemes";
 import { getSpeakerMaterialName } from "../materials/speakerMaterialSlots";
 import { speaker2ColorSchemes } from "../materials/speaker2ColorSchemes";
